b483b7bcaf4192c40aff5f17c6f85d87d550e070
Mugi <g.mugi93140@gmail.com>
2021-04-27 10:02:17 +0200

    changer le nom de game et commentaire

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -6,6 +6,12 @@ class Utils extends React.Component {
     rndCarte() {
         let rndCarteTemp = "";
         let rndNumTemp = 0;
+        const cardArray = [
+            "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
+            "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
+            "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
+            "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
+
 
         rndNumTemp = Math.floor(Math.random() * 53);
 
