fabab5ea8f088e3815442e9e8de5433fb1483785
Mugi <g.mugi93140@gmail.com>
2021-04-27 10:31:30 +0200

    Utils.js import methode

--- src/Table.js
+++ src/Table.js
@@ -5,6 +5,8 @@ import Button from './componant/button/Button.jsx'
 import Cartes from "./views/carte/Cartes";
 import Game from './views/Play/StartGame.jsx'
 import Utils from "./utils/Utils.js"
+import {rndCarte} from "./utils/Utils.js"
+import {transformCardIntoInt} from "./utils/Utils"
 
 
 
@@ -30,32 +32,32 @@ class Table extends React.Component {
     }
   }
 
-  rndCarte() {
-    const cardArray = [
-      "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
-      "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
-      "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
-      "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
+  // rndCarte() {
+  //   const cardArray = [
+  //     "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
+  //     "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
+  //     "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
+  //     "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
     
-    let rndCarteTemp = "";
-    let rndNumTemp = 0;
+  //   let rndCarteTemp = "";
+  //   let rndNumTemp = 0;
 
-    rndNumTemp = Math.floor(Math.random() * 53);
+  //   rndNumTemp = Math.floor(Math.random() * 53);
 
-    if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
+  //   if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
 
-    rndCarteTemp = cardArray[rndNumTemp - 1];
+  //   rndCarteTemp = cardArray[rndNumTemp - 1];
 
-    return rndCarteTemp
-  }
+  //   return rndCarteTemp
+  // }
 
   onClickStop = () => {
     // la valeur des 2 premieres cartes 
-    const cardSelectedDealer = this.rndCarte()
-    const cardSelectedDealer2 = this.rndCarte()
+    const cardSelectedDealer = rndCarte()
+    const cardSelectedDealer2 = rndCarte()
 
-    const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
-    const valueCarteDealer2 = this.transformCardIntoInt(cardSelectedDealer2.split("")[0])
+    const valueCarteDealer = transformCardIntoInt(cardSelectedDealer.split("")[0])
+    const valueCarteDealer2 = transformCardIntoInt(cardSelectedDealer2.split("")[0])
 
     const cardsDealer = [cardSelectedDealer, cardSelectedDealer2]
 
@@ -67,8 +69,8 @@ class Table extends React.Component {
     }
     // condition pour gagner
     while (dealerValue < 17) {
-      const cardSelectedDealer = this.rndCarte()
-      const valueCarteDealer = this.transformCardIntoInt(cardSelectedDealer.split("")[0])
+      const cardSelectedDealer = rndCarte()
+      const valueCarteDealer = transformCardIntoInt(cardSelectedDealer.split("")[0])
 
       cardsDealer.push(cardSelectedDealer)
 
@@ -114,8 +116,8 @@ class Table extends React.Component {
 
   onClickGive = () => {
     // carte du joueur 
-    const cardSelected = this.rndCarte()
-    const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
+    const cardSelected = rndCarte()
+    const valueCarte = transformCardIntoInt(cardSelected.split("")[0])
     const totalPlayerValue = this.state.counterPlayer + valueCarte
 
     this.setState({
@@ -124,20 +126,20 @@ class Table extends React.Component {
     })
   }
 
-  transformCardIntoInt(cardValue) {
-    if (cardValue === "K" || cardValue === "Q" || cardValue === "J" || cardValue === "A" || cardValue === "0") {
-      cardValue = "10"
-    }
+  // transformCardIntoInt(cardValue) {
+  //   if (cardValue === "K" || cardValue === "Q" || cardValue === "J" || cardValue === "A" || cardValue === "0") {
+  //     cardValue = "10"
+  //   }
 
-    return parseInt(cardValue)
-  }
+  //   return parseInt(cardValue)
+  // }
 
   startGame = () => {
-    const cardSelected = this.rndCarte()
-    const cardSelected2 = this.rndCarte()
+    const cardSelected = rndCarte()
+    const cardSelected2 = rndCarte()
 
-    const valueCarte = this.transformCardIntoInt(cardSelected.split("")[0])
-    const valueCarte2 = this.transformCardIntoInt(cardSelected2.split("")[0])
+    const valueCarte = transformCardIntoInt(cardSelected.split("")[0])
+    const valueCarte2 = transformCardIntoInt(cardSelected2.split("")[0])
 
     const firstPlayerValue = valueCarte + valueCarte2
 
